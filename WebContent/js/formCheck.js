9902960038caba82d20a34894cfbb68b4e918eaf
leemanni <cjffydahs@naver.com>
2021-10-07 22:32:57 +0900

    변수타입 수정

--- WebContent/js/formCheck.js
+++ WebContent/js/formCheck.js
@@ -1,7 +1,7 @@
 $(()=>{
 	$('.login-form').submit(function(e) {
-		var id = $.trim($('#id').val()).length;
-		var pw = $.trim($('#pw').val()).length;
+		const id = $.trim($('#id').val()).length;
+		const pw = $.trim($('#pw').val()).length;
 		if(id==0 || pw ===0){
 			alert('아이디와 패스워드를 입력하세요')
 			e.preventDefault();
@@ -10,3 +10,5 @@ $(()=>{
 		}
 	})
 } )
+
+
